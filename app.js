0c247a4b4af91ea0a29b220ce175d3bd1a24fc95
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-20 21:47:56 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
