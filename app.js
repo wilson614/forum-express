5f3612382665b9bdd24206656515801bc754be37
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-22 17:10:15 +0800

    Add form validator and flash message

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -7,6 +9,13 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
