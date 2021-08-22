fef6f4f508b11e4cabc7715916c63d2d668ad304
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-22 16:35:15 +0800

    Add user signup function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
