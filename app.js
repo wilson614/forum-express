f23d36fa3d2bb985e1c7f6a2ba1140e98f6dd0a0
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-22 17:30:07 +0800

    Add user signin function with passport.js

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
+
 const db = require('./models')
 const app = express()
 const port = 3000
+const passport = require('./config/passport')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -22,6 +26,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
