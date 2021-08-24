cfd28d142de15449dc54253451b2fe4ee1e23abc
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-25 01:33:05 +0800

    Modify with _helpers.js for assignment preparation

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,19 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 const multer = require('multer')
+const helpers = require('../_helpers')
 const upload = multer({ dest: 'temp/' })
 
 module.exports = (app, passport) => {
   const authenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (helpers.ensureAuthenticated(req)) {
       return next()
     }
     res.redirect('/signin')
   }
   const authenticatedAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      if (req.user.isAdmin) { return next() }
+    if (helpers.ensureAuthenticated(req)) {
+      if (helpers.getUser(req).isAdmin) { return next() }
       return res.redirect('/')
     }
     res.redirect('/signin')
