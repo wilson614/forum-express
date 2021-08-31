70e6c62eb8135b87fb01bde87f9326b0d493c6f0
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-31 18:22:48 +0800

    Show category name on pages

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,11 +2,16 @@ const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models')
 const Restaurant = db.Restaurant
+const Category = db.Category
 const User = db.User
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
   },
@@ -53,8 +58,10 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/restaurant', { restaurant })
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    }).then(restaurant => {
+      return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
     })
   },
   editRestaurant: (req, res) => {
